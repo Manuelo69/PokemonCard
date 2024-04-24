afc59f85220d1ca7c717512e4611b1b87597d600
manuelo69 <ichigo2418@gmail.com>
2024-04-24 09:11:43 +0200

    primera aplicación de estilos de carta

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -9,15 +9,19 @@ const PokemonCard = ({
   defense,
 }) => {
   return (
-    <div className="pokemon-card flex justify-center items-center flex-col">
-      <img src={sprite} alt={name} />
+    <div className="pokemon-card flex items-center flex-col bg-white shadow-xl rounded-2xl h-80 w-72 justify-around">
+      <div>
+        <img src={sprite} alt={name} />
+      </div>
       <p>
         {name} {hp}hp
       </p>
       <p>Exp: {exp}</p>
-      <p className="line">
-        {attack} Ataque | {specialAttack} Ataque especial | {defense} Defensa
-      </p>
+      <div className="w-40 text-sm flex flex-row">
+        <p>{attack} Ataque</p>
+        <p>{specialAttack} Ataque especial</p>
+        <p>{defense} Defensa</p>
+      </div>
     </div>
   );
 };
